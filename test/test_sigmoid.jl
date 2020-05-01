ba0eb501069a9494ce894e03195d2cf7cbc24cea
Jukka Aho <ahojukka5@gmail.com>
2020-05-01 23:49:11 +0700

    Test sigmoid with SVector

--- test/test_sigmoid.jl
+++ test/test_sigmoid.jl
@@ -1,6 +1,7 @@
-using Test
+using Test, StaticArrays
 using LogisticRegression: sigmoid
 
 @test isapprox(sigmoid(0.0), 0.5);
 @test isapprox(sigmoid(2.0), 0.88079708);
 @test isapprox(sigmoid([0.0, 2.0]), [0.5, 0.88079708])
+@test isapprox(sigmoid(@SVector [0.0, 2.0]), [0.5, 0.88079708])
