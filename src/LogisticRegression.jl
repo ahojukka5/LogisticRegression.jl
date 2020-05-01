cbdcf69c79dac1b7600ff472e8983f1ac3c8883c
Jukka Aho <ahojukka5@gmail.com>
2020-05-01 23:46:38 +0700

    Make sigmoid to work with vectors

--- src/LogisticRegression.jl
+++ src/LogisticRegression.jl
@@ -4,4 +4,8 @@ function sigmoid(z::Number)
     return 1.0 / (1.0 + exp(-z))
 end
 
+function sigmoid(z::Vector)
+    return sigmoid.(z)
+end
+
 end # module
--- test/test_sigmoid.jl
+++ test/test_sigmoid.jl
@@ -3,3 +3,4 @@ using LogisticRegression: sigmoid
 
 @test isapprox(sigmoid(0.0), 0.5);
 @test isapprox(sigmoid(2.0), 0.88079708);
+@test isapprox(sigmoid([0.0, 2.0]), [0.5, 0.88079708])
